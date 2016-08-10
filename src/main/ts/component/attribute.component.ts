9ab63da9082eec76729af4b56c59a5fa14b07486
Stephen Gream <stephen.gream@gmail.com>
2016-08-10 23:03:30 +1000

    Recentered text in attribute containers

--- src/main/ts/component/attribute.component.ts
+++ src/main/ts/component/attribute.component.ts
@@ -6,9 +6,9 @@ import {Component, Input} from '@angular/core';
 @Component({
     selector: 'attribute',
     template: `
-        <div class="attribute-component col-xs-4 col-sm-2 col-md-2 col-lg-2">
-            <p class="attribute-magnitude">{{magnitude}}</p>
-            <p class="attribute-name">{{attributeName}}</p>
+        <div class="attribute-component col-xs-4 col-sm-2 col-md-2 col-lg-2 container-fluid">
+            <p class="attribute-magnitude col-xs-12">{{magnitude}}</p>
+            <p class="attribute-name col-xs-12">{{attributeName}}</p>
         </div>
     `
 })
